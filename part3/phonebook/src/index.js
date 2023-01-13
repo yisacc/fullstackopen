08154f929069504cf92673d1f2c62577483900c8
Yisacc Aberham <isaccab2019@gmail.com>
2023-01-13 20:33:10 +0300

    phonebook deployed to fly

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const app=express()
 
 app.use(express.json())
-
+app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
@@ -83,7 +83,7 @@ app.post('/api/persons',(request,response)=>{
     }
     response.json(person)
 })
-const PORT=3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
